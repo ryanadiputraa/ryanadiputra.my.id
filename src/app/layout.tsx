d8e5a5d86910c34cf3c3775e8b31b94258a82c62
Ryan Adi Putra <ryannadiputraa@gmail.com>
2024-07-23 21:56:20 +0800

    fix(layout): min height layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import { Manrope } from "next/font/google";
+import { cookies } from "next/headers";
 
 import { DEFAULT_THEME } from "@/constants";
 import { type Theme } from "@/types";
 
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
-import { cookies } from "next/headers";
 
 import "./globals.css";
 
@@ -30,7 +30,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 className={`${font.className} bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-50 transition-colors font-thin`}
             >
                 <Header initialTheme={initialTheme} />
-                <div className="min-h-[95vh] pt-28 px-4 w-full max-w-3xl mx-auto">{children}</div>
+                <div className="min-h-[94vh] pt-28 px-4 w-full max-w-3xl mx-auto">{children}</div>
                 <Footer />
             </body>
         </html>
